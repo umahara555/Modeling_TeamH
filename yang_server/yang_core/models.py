d81fad5a845b3a67c3cf66cce02e594c3ec0d1ed
Uehara Yuuku <e175732@ie.u-ryukyu.ac.jp>
2018-11-26 11:11:11 +0900

    コア部分になるアプリケーションを生成

---
+++ yang_server/yang_core/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=256)
--- yang_server/yang_server/settings.py
+++ yang_server/yang_server/settings.py
@@ -17,6 +17,7 @@ ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
+    'yang_core.apps.YangCoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
