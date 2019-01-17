ae4dffeab1e0bc29d9eea484d76be1d1de3c89cf
Uehara Yuuku <e175732@ie.u-ryukyu.ac.jp>
2019-01-17 22:06:28 +0900

    ユーザー周りを整備

--- yang_server/yang_core/models.py
+++ yang_server/yang_core/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     email = models.EmailField(max_length=256)
     name = models.CharField(max_length=256)
     password = models.CharField(max_length=256)
-    token = models.CharField(max_length=256)
+    token = models.CharField(max_length=32)
 
     def __str__(self):
         return self.name
--- yang_server/yang_core/serializer.py
+++ yang_server/yang_core/serializer.py
@@ -2,7 +2,21 @@ from rest_framework import serializers
 
 from .models import User
 
+import secrets
+import hashlib
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('name',)
+        fields = ('email', 'name', 'password', 'token',)
+        read_only_fields = ('token',)
+
+    def create(self, validated_data):
+        user = User(
+            email = validated_data['email'],
+            name = validated_data['name'],
+            password = hashlib.sha256(validated_data['password'].encode("utf-8")).hexdigest(),
+            token = secrets.token_hex(16)
+        )
+        user.save()
+        return user
