38c05b879f27754e600b2c7c83aa68574058588e
Hiroto Shimabukuro <e175765@ie.u-ryukyu.ac.jp>
2019-01-20 17:15:10 +0900

    UIchanges

---
+++ yang/yang/AddController.swift
@@ -0,0 +1,30 @@
+//
+//  AddController.swift
+//  yang
+//
+//  Created by Hiroto Shimabukuro on 2018/12/18.
+//  Copyright © 2018 玉橋優太朗. All rights reserved.
+//
+
+import UIKit
+
+class AddController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
